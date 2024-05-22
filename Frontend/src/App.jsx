f01322c9a2fbae984247c15f3ab8a02d92318995
GideonMissoy <giddykips2030@gmail.com>
2024-05-22 20:06:17 +0300

    Updates to the frontend.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import React from "react"
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <h1>Testing React Code</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
